0d6d08e1a7d24b8e04bae16b6a628698ba62d7fd
JM Versteeg <janmerijnversteeg@gmail.com>
2017-04-06 22:13:23 +0200

    add deeper integration with child_process by allowing passing a ChildProcess instance as the second argument to the Process constructor

--- Process.js
+++ Process.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const defaults     = require('defa');
+const ChildProcess = require('child_process').ChildProcess;
 const EventEmitter = require('events').EventEmitter;
 const Promise      = require('bluebird');
 
@@ -9,22 +10,34 @@ const Promise      = require('bluebird');
  */
 class Process extends EventEmitter {
 
+    /**
+     * @param {String} command
+     * @param {Object|ChildProcess} options
+     */
     constructor(command, options = {}) {
 
         super();
 
-        options = defaults({}, options, {
-            cwd:    null,
-            host:   'localhost',
-            stdout: null,
-            stderr: null
-        });
-
         this.command = command;
-        this.cwd     = options.cwd;
-        this.host    = options.host;
-        this.stdout  = options.stdout;
-        this.stderr  = options.stderr;
+
+        if (!(options instanceof ChildProcess)) {
+            options = defaults({}, options, {
+                cwd:    null,
+                host:   'localhost',
+                stdout: null,
+                stderr: null
+            });
+        }
+
+        this.host   = options.host;
+        this.cwd    = options.cwd;
+        this.stdout = options.stdout;
+        this.stderr = options.stderr;
+
+        if (options instanceof ChildProcess) {
+            this.childProcess = options;
+            this.listen(options);
+        }
 
         this.running = true;
 
@@ -32,6 +45,22 @@ class Process extends EventEmitter {
         this.signal = null; // exit signal
     }
 
+    /**
+     * Listen to the "close" event on the given child process.
+     * Note this method is automatically invoked if you supply a ChildProcess instance
+     * to the constructor as the second parameter.
+     * @param childProcess
+     */
+    listen(childProcess) {
+        childProcess.on('exit', (code, signal) => this.done(code, signal));
+        return this;
+    }
+
+    /**
+     * Indicate that the process has exited.
+     * @param {int|null} code
+     * @param {string|null} signal
+     */
     done(code, signal) {
         this.running = false;
 
@@ -41,6 +70,11 @@ class Process extends EventEmitter {
         this.emit('done', {code, signal});
     }
 
+    /**
+     * Returns a promise for the completion of the process
+     * The promise will be resolved with {code, signal} when the process closes.
+     * @returns {Promise}
+     */
     await() {
         return new Promise(resolve => this.on('done', resolve));
     }
