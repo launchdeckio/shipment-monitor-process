08a7f4228e003bab944915ff2535b64dff5d50fb
JM Versteeg <janmerijnversteeg@gmail.com>
2017-04-06 22:42:15 +0200

    fix problem in CLI reducer

--- eventReducers.js
+++ eventReducers.js
@@ -25,7 +25,10 @@ module.exports = (parser, options = {}) => {
 
             [events.STDERR]: stderr => process.stderr.write(stderrPrefix + stderr),
             [events.STDOUT]: stdout => process.stdout.write(stdoutPrefix + stdout),
-            [events.EXIT]:   ({code, signal}, info) => {
+            [events.EXIT]:   (exit, info) => {
+
+                const code   = exit.get('code');
+                const signal = exit.get('signal');
 
                 const notation = code !== null ?
                     (code === 0 ? chalk.green(code) : chalk.red(code)) :
